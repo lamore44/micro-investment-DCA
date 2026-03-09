040c2289515b459f55ce4b34fb2b1397416feb17
adhietya <laluadittyapratamajelindra@gmail.com>
2026-03-09 15:02:14 +0800

    Tambahkan synchronization supaya hanya satu request yang lewat setiap interval

--- core/core-network/src/main/kotlin/com/example/microdca/core/network/interceptors/RateLimitInterceptor.kt
+++ core/core-network/src/main/kotlin/com/example/microdca/core/network/interceptors/RateLimitInterceptor.kt
@@ -3,19 +3,21 @@
 import okhttp3.Interceptor
 import okhttp3.Response
 import javax.inject.Inject
-import java.util.concurrent.TimeUnit
 
 class RateLimitInterceptor @Inject constructor() : Interceptor {
     private var lastRequestTime = 0L
     private val minIntervalMs = 100L // max 10 requests per second
+    private val lock = Any()
 
     override fun intercept(chain: Interceptor.Chain): Response {
-        val now = System.currentTimeMillis()
-        val elapsed = now - lastRequestTime
-        if (elapsed < minIntervalMs) {
-            Thread.sleep(minIntervalMs - elapsed)
+        synchronized(lock){
+            val now = System.currentTimeMillis()
+            val elapsed = now - lastRequestTime
+            if (elapsed < minIntervalMs) {
+                Thread.sleep(minIntervalMs - elapsed)
+            }
+            lastRequestTime = System.currentTimeMillis()
         }
-        lastRequestTime = System.currentTimeMillis()
         return chain.proceed(chain.request())
     }
 }
